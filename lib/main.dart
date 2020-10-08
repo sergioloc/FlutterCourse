f15943d03b7c9f2f1345b56ab3143f7f090bdbf5
Sergio López Ceballos <sergioloc2@gmail.com>
2020-10-08 13:48:14 +0200

    Column

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Course',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Course"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.bedtime),
-            onPressed: add,
-          ),
-          IconButton(
-            icon: Icon(Icons.dashboard),
-            onPressed: add,
-          )
-        ]
-      ),
-      body: Center(
-        child: IconButton(
-          icon: Icon(Icons.beach_access, color: Colors.blue),
-          onPressed: () {
-            print("Hola");
-          },
-          iconSize: 200,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Course"),
         ),
-      ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.orange,
+                height: 100,
+                width: 100,
+              ),
+              Text("Hi")
+            ],
+          ),
+        )
+      )
     );
-  }
-
-  void add(){
-    print("Hola");
   }
 }
