7d0cbd397f5c31568fa69cb93eaef6089bb982e3
Sergio López Ceballos <sergioloc2@gmail.com>
2020-10-08 13:18:21 +0200

    Image from network

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Course',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
-        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -21,15 +18,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget{
-
+class MyHomePage extends StatelessWidget {
   MyHomePage();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text("Bienvenido a Flutter", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),),
+        child: Image.network(
+          "https://cdn.iconscout.com/icon/free/png-512/flutter-2038877-1720090.png",
+          height: 100,
+        ),
       ),
     );
   }
